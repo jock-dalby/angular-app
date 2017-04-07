94f655d96f2d385dbd770b6ef2c539b05145deda
Jock Dalby <jock.dalby@education.govt.nz>
2017-04-08 10:06:09 +1200

    organise components to reflect basic project structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,11 @@ import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
-import { ShoppingListEditComponent } from './shopping-list-edit/shopping-list-edit.component';
-import { RecipeBookComponent } from './recipe-book/recipe-book.component';
-import { RecipeDetailComponent } from './recipe-detail/recipe-detail.component';
-import { RecipeListComponent } from './recipe-list/recipe-list.component';
-import { RecipeItemComponent } from './recipe-item/recipe-item.component';
+import { ShoppingListEditComponent } from './shopping-list/shopping-list-edit/shopping-list-edit.component';
+import { RecipesComponent } from './recipes/recipes.component';
+import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
+import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
+import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
 
 @NgModule({
   declarations: [
@@ -18,7 +18,7 @@ import { RecipeItemComponent } from './recipe-item/recipe-item.component';
     HeaderComponent,
     ShoppingListComponent,
     ShoppingListEditComponent,
-    RecipeBookComponent,
+    RecipesComponent,
     RecipeDetailComponent,
     RecipeListComponent,
     RecipeItemComponent
