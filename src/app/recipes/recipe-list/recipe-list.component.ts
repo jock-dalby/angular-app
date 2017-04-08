a75d3e0ee490576060af4e430b607425c6b1faeb
Jock Dalby <jock.dalby@education.govt.nz>
2017-04-09 07:27:21 +1200

    add single recipe row

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,12 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Recipe } from '../recipe.model'; // Import class 'Recipe' from model
+
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-  recipes: [];
+  recipes: Recipe[] = [ // Shows the 'recipes' property, is an array of objects, each of the type 'Recipe'.
+      new Recipe('Test Recipe', 'This is a test', 'http://maxpixel.freegreatpicture.com/static/photo/640/Cheese-Lunch-Slice-Pizza-Toppings-Italian-Dinner-329523.jpg') // Using the 'new' keyword we can create a new object based on the Recipe blueprint by calling the constructor function.
+  ];
 
   constructor() { }
 
--- src/app/recipes/recipe.model.ts
+++ src/app/recipes/recipe.model.ts
@@ -1,11 +1,18 @@
-export class Recipe {
-    public name: string;
-    public description: string;
+export class Recipe { // Export TS class
+
+    public name: string; // Public accessor and can be accessed from outside if using this as an instantiated object
+    public description: string; // format => accessor property-name: type;
     public imagePath: string;
 
     constructor(name: string, desc: string, imagePath: string) {
         this.name = name;
         this.description = desc;
         this.imagePath = imagePath;
-    }
+    } // Constructors allow us to instantiate the recipe object using the Recipe keyword.
+
 }
+
+// A modal is blueprint for objects to be created, here achieved using a vanilla typescript class.
+// When the class is instatiated we can create new objects based on this class which defines what a single recipe should look like.
+
+// Instantiate => to represent (an abstraction) by a concrete instance.
