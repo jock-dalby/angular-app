5c6b8df33466a67eedb9f1a7c7b739e5b1a30da2
Jock Dalby <jockdalby86@gmail.com>
2017-04-19 08:39:54 +1200

    add ingredients array to recipe items

--- src/app/recipes/recipe.model.ts
+++ src/app/recipes/recipe.model.ts
@@ -1,13 +1,17 @@
+import { Ingredient } from '../shared/ingredient.model';
+
 export class Recipe { // Export TS class
 
     public name: string; // Public accessor and can be accessed from outside if using this as an instantiated object
     public description: string; // format => accessor property-name: type;
     public imagePath: string;
+    public ingredients: Ingredient[];
 
-    constructor(name: string, desc: string, imagePath: string) {
+    constructor(name: string, desc: string, imagePath: string, ingredients: Ingredient[]) {
         this.name = name;
         this.description = desc;
         this.imagePath = imagePath;
+        this.ingredients = ingredients;
     } // Constructors allow us to instantiate the recipe object using the Recipe keyword.
 
 }
--- src/app/recipes/service.ts
+++ src/app/recipes/service.ts
@@ -1,14 +1,34 @@
 import { EventEmitter } from '@angular/core';
 
 import { Recipe } from './recipe.model';
+import { Ingredient } from '../shared/ingredient.model';
 
 export class RecipesService {
 
   recipeSelected = new EventEmitter<Recipe>();
 
   recipes: Recipe[] = [ // Shows the 'recipes' property, is an array of objects, each of the type 'Recipe'.
-    new Recipe('Cheese Pizza', 'Delicious as bro!', 'http://maxpixel.freegreatpicture.com/static/photo/640/Cheese-Lunch-Slice-Pizza-Toppings-Italian-Dinner-329523.jpg'), // Using the 'new' keyword we can create a new object based on the Recipe blueprint by calling the constructor function.
-    new Recipe('Chocolate Brownie', 'Lovely bit of choc', 'https://upload.wikimedia.org/wikipedia/commons/thumb/b/bc/Whiskey_Chocolate_Cake.jpg/800px-Whiskey_Chocolate_Cake.jpg')
+    // Using the 'new' keyword we can create a new object based on the Recipe blueprint by calling the constructor function.
+    new Recipe(
+        'Cheese Pizza',
+        'Delicious as bro!',
+        'http://maxpixel.freegreatpicture.com/static/photo/640/Cheese-Lunch-Slice-Pizza-Toppings-Italian-Dinner-329523.jpg',
+        [
+          new Ingredient('bread', 6),
+          new Ingredient('cheese', 4),
+          new Ingredient('tomato', 6)
+        ]
+    ),
+    new Recipe(
+        'Chocolate Brownie',
+        'Lovely bit of choc',
+        'https://upload.wikimedia.org/wikipedia/commons/thumb/b/bc/Whiskey_Chocolate_Cake.jpg/800px-Whiskey_Chocolate_Cake.jpg',
+        [
+          new Ingredient('chocolate', 6),
+          new Ingredient('flour', 4),
+          new Ingredient('butter', 6)
+        ]
+    )
   ];
 
   constructor(
