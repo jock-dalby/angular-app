8ff9555b88319dfc044292e5a8a15995d8ae0ed2
Jock Dalby <jockdalby86@gmail.com>
2017-05-01 17:29:02 +1200

    add new ingredient row

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -32,6 +32,14 @@ export class RecipeEditComponent implements OnInit {
     );
   }
 
+  onAddIngredient() {
+    (<FormArray>this.recipeForm.get('ingredients'))
+      .push(new FormGroup({
+        'name': new FormControl(),
+        'amount': new FormControl()
+      }));
+  }
+
   onSubmit() {
     console.log(this.recipeForm);
   }
